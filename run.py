1d64cffea52e1dd56246d6b8f585004b38e7abb7
Sahil-1727 <117593753+Sahil-1727@users.noreply.github.com>
2026-03-20 10:26:28 +0530

    Remove db.create_all conflict

--- run.py
+++ run.py
@@ -2,9 +2,5 @@ from app import create_app, db
 
 app = create_app()
 
-# Add these two lines right here!
-with app.app_context():
-    db.create_all()
-
 if __name__ == "__main__":
     app.run(debug=True)
